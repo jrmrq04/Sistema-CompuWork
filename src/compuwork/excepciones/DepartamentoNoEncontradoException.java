8b7f8923f6afdb99313e2b0859483ab36ad9423c
jrmrq04 <jairomarquez0416@gmail.com>
2026-03-13 19:47:07 -0500

    Initial commit

---
+++ src/compuwork/excepciones/DepartamentoNoEncontradoException.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package compuwork.excepciones;
+
+/**
+ *
+ * @author jrmrq
+ */
+public class DepartamentoNoEncontradoException extends Exception {
+    public DepartamentoNoEncontradoException(String mensaje) {
+        super(mensaje);
+    }
+}
